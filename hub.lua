2b16c01dd5cad1127b8180632886aeab36621a1c
user404-hub <impactg583@gmail.com>
2026-04-04 23:59:07 -0300

    distrito

--- hub.lua
+++ hub.lua
@@ -27,13 +27,16 @@ end
 waitForGameToFullyLoad()
 
 -- Configurações
-local HUB_VERSION = "0.1.3"
+local HUB_VERSION = "0.2"
 local SCRIPT_DELAY = 2
 
 -- SCRIPTS POR JOGO (IDs numéricos)
 local scripts = {
     [7326934954] = { -- 99 Nights in the Forest
         "https://raw.githubusercontent.com/caomod2077/Script/refs/heads/main/FoxnameHub.lua"
+    },
+    [6739698191] = { -- Violence-District
+        "https://api.luarmor.net/files/v3/loaders/fda9babd071d6b536a745774b6bc681c.lua"
     }
 }
 
